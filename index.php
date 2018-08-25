0e6a38f84b1401bbc3858e2d0e44d23ddba90b51
Andre Leonardo <andreleonardo21@hotmail.com>
2018-08-24 22:29:43 -0300

    Configuração de autoload do composer

--- index.php
+++ index.php
@@ -6,10 +6,13 @@ $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+$app->get('/', function()
+{
+    $sql = new Eletronic\DB\Sql();
     
-	echo "OK";
+    $results = $sql->select("SELECT * FROM tb_users");
 
+    echo json_encode($results);
 });
 
 $app->run();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInita724b403d678de644798fa8b5343e807
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/eletronic/classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'E' => 
+        array (
+            'Eletronic\\' => 10,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Eletronic\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/eletronic/classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInita724b403d678de644798fa8b5343e807
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInita724b403d678de644798fa8b5343e807::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInita724b403d678de644798fa8b5343e807::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInita724b403d678de644798fa8b5343e807::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInita724b403d678de644798fa8b5343e807::$prefixesPsr0;
             $loader->classMap = ComposerStaticInita724b403d678de644798fa8b5343e807::$classMap;
 
