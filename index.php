023c74a0ab9b9affc71567adbf3d730c4bc1c3b9
Andre Leonardo <andreleonardo21@hotmail.com>
2018-08-24 23:32:44 -0300

    Criado classe page.php e header, index e footer

--- index.php
+++ index.php
@@ -2,17 +2,19 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Eletronic\Page;
+
+$app = new Slim;
 
 $app->config('debug', true);
 
 $app->get('/', function()
 {
-    $sql = new Eletronic\DB\Sql();
-    
-    $results = $sql->select("SELECT * FROM tb_users");
+    $page = new Page();
 
-    echo json_encode($results);
+    $page->setTpl("index");
+
 });
 
 $app->run();
---
+++ vendor/eletronic/classes/src/page.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Eletronic;
+
+use Rain\Tpl;
+
+class Page
+{
+    private $tpl;
+    private $options = [];
+    private $defaults =[
+        "data"=>[]
+    ];
+    public function __construct($opcoes = [])
+    {
+        $this->options = array_merge($this->defaults, $opcoes);
+
+        $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+            "debug"         => false
+           );
+
+           Tpl::configure($config);
+
+           $this->tpl = new Tpl;
+
+           $this->setData($this->options["data"]);
+
+           $this->tpl->draw("header");
+
+    }
+
+    private function setData($data = [])
+    {
+        foreach ($this->options["data"] as $key => $value)
+           {
+               $this->tpl->assign($key, $value);
+           }
+    }
+
+    public function setTpl($name, $data = [], $returnHtml = false)
+    {
+        $this->setData($data);
+
+        return $this->tpl->draw($name, $returnHtml);
+    }
+
+
+    public function __destruct()
+    {
+        $this->tpl->draw("footer");
+    }
+}
+
+
+?>
